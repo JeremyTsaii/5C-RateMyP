80da03d292bd8b20c90e50c0a52a8d574f215190
Jeremy <jetsai@hmc.edu>
2019-08-16 23:38:26 -0700

    background script unecessary/causes lag

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+//NOTICE:
+//background script removed from extension
+//for some reason, having this background script causes major lag (not sure why)
+
 //receives message from content script and activates extension icon 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.type === 'showPageAction') {
